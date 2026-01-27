da0d18175c16fb37d7b4dae146e8de109dcf3e23
jamainefacey-blip <jamainefacey@icloud.com>
2026-01-27 15:19:35 +0000

    Fix Sidebar import path

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,5 +1,5 @@
 // src/app/admin/layout.js
-import Sidebar from "@/components/common/Sidebar";
+import Sidebar from "../../components/common/Sidebar";
 
 export default function AdminLayout({ children }) {
   return (
